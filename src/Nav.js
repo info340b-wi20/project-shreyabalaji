6ef88adf0ded228d21830f92f7b63fc6be5b9ccd
Shreya Balaji <shreyb3@uw.edu>
2020-03-02 09:34:15 -0800

    nav bar fixed

--- src/Nav.js
+++ src/Nav.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-// import FontAwesome from 'react-fontawesome'
-// import faStyles from 'font-awesome/css/font-awesome.css'
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FontAwesome from 'react-fontawesome'
+//import faStyles from 'font-awesome/css/font-awesome.css'
+//import { FontAwesomeIcon } from '@fontawesome/react-fontawesome'
 
 export default class Nav extends Component { //export allows other things to use this class.
     render() {
         return (
-            // <head>
-            //     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-            // </head>
+            
+             
             <nav>
                 <ul>
-                    {/* <li><a href="index.html"><FontAwesome className="fas fa-home" aria-label="home feed"/></a></li>
+                    <li><a href="index.html"><FontAwesome className="fas fa-home" aria-label="home feed"/></a></li>
                     <li><a href="likes.html"><FontAwesome className="far fa-heart" aria-label="Your likes"/></a></li>
                     <li><a href="messaging.html"><FontAwesome className="fas fa-comments" aria-label="messages"/></a></li>
-                    <li><a href="profile.html"><FontAwesome className="fas fa-user" aria-label="profile"/></a></li> */}
+                    <li><a href="profile.html"><FontAwesome className="fas fa-user" aria-label="profile"/></a></li>
                 </ul>
             </nav>
         );
